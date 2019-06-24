eb26f88abc24147b8e0eb9cc20b61247e3f9b9e7
Christos Christidis <christos.christidis86@gmail.com>
2019-06-24 19:19:07 +0300

    getting current place w Places API

--- app/src/main/java/com/advanced/walkmyandroid/MainActivity.java
+++ app/src/main/java/com/advanced/walkmyandroid/MainActivity.java
@@ -21,19 +21,37 @@ import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.model.PlaceLikelihood;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceRequest;
+import com.google.android.libraries.places.api.net.FindCurrentPlaceResponse;
+import com.google.android.libraries.places.api.net.PlacesClient;
 
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+// SOS: Followed instructions in following link to replace deprecated API w the new one:
+// https://developers.google.com/places/android-sdk/client-migration
 public class MainActivity extends AppCompatActivity implements FetchAddressTask.OnTaskCompleted {
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
+
     private static final int GRANTED = PackageManager.PERMISSION_GRANTED;
+    private static final String LOCATION_PERMISSION = Manifest.permission.ACCESS_FINE_LOCATION;
 
     private static final String STATE_TRACKING_LOCATION = "state_tracking_location";
 
     private TextView mLocationTextView;
+    private ImageView mImageView;
     private AnimatorSet mRotateAnim;
     private Button mButton;
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private PlacesClient mPlacesClient;
     private boolean mTrackingLocation;
     private LocationCallback mLocationCallback;
 
@@ -43,15 +61,18 @@ public class MainActivity extends AppCompatActivity implements FetchAddressTask.
         setContentView(R.layout.activity_main);
 
         mLocationTextView = findViewById(R.id.textview_location);
-        ImageView imageView = findViewById(R.id.imageview_android);
+        mImageView = findViewById(R.id.imageview_android);
         mButton = findViewById(R.id.button_location);
 
         mRotateAnim = (AnimatorSet) AnimatorInflater.loadAnimator(this, R.animator.rotate);
-        mRotateAnim.setTarget(imageView);
+        mRotateAnim.setTarget(mImageView);
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
-        if (savedInstanceState != null){
+        Places.initialize(this, getString(R.string.places_key));
+        mPlacesClient = Places.createClient(this);
+
+        if (savedInstanceState != null) {
             mTrackingLocation = savedInstanceState.getBoolean(STATE_TRACKING_LOCATION);
         }
 
@@ -81,19 +102,22 @@ public class MainActivity extends AppCompatActivity implements FetchAddressTask.
     }
 
     private void startTrackingLocation() {
-        if (ActivityCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_FINE_LOCATION) != GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, LOCATION_PERMISSION) != GRANTED) {
             ActivityCompat.requestPermissions(this,
-                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                    LOCATION_PERMISSION_REQUEST_CODE);
+                    new String[]{LOCATION_PERMISSION}, LOCATION_PERMISSION_REQUEST_CODE);
         } else {
             mFusedLocationClient.requestLocationUpdates(getLocationRequest(),
                     mLocationCallback, null);
-        }
 
-        mRotateAnim.start();
-        mTrackingLocation = true;
-        mButton.setText(getString(R.string.stop_tracking_location));
+            mLocationTextView.setText(getString(R.string.address_text,
+                    getString(R.string.loading),
+                    getString(R.string.loading),
+                    new Date()));
+
+            mRotateAnim.start();
+            mTrackingLocation = true;
+            mButton.setText(getString(R.string.stop_tracking_location));
+        }
     }
 
     private void stopTrackingLocation() {
@@ -127,9 +151,71 @@ public class MainActivity extends AppCompatActivity implements FetchAddressTask.
     }
 
     @Override
-    public void onTaskCompleted(String result) {
+    public void onTaskCompleted(final String result) {
         if (mTrackingLocation) {
-            mLocationTextView.setText(getString(R.string.address_text, result, System.currentTimeMillis()));
+            // SOS: we check again because it's required by findCurrentPlace
+            if (ActivityCompat.checkSelfPermission(this, LOCATION_PERMISSION) != GRANTED) {
+                return;
+            }
+
+            // SOS: Select which place data to return (billing varies...)
+            List<Place.Field> placeFields = Collections.singletonList(Place.Field.NAME);
+
+            FindCurrentPlaceRequest request = FindCurrentPlaceRequest.builder(placeFields).build();
+            mPlacesClient.findCurrentPlace(request).addOnSuccessListener(new OnSuccessListener<FindCurrentPlaceResponse>() {
+                @Override
+                public void onSuccess(FindCurrentPlaceResponse response) {
+                    double maxLikelihood = 0;
+                    Place currentPlace = null;
+                    for (PlaceLikelihood placeLikelihood : response.getPlaceLikelihoods()) {
+                        if (maxLikelihood < placeLikelihood.getLikelihood()) {
+                            maxLikelihood = placeLikelihood.getLikelihood();
+                            currentPlace = placeLikelihood.getPlace();
+                        }
+                    }
+
+                    if (currentPlace != null) {
+                        mLocationTextView.setText(getString(R.string.address_text,
+                                currentPlace.getName(), result,
+                                System.currentTimeMillis()));
+
+                        setPlaceTypeImage(currentPlace);
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    mLocationTextView.setText(getString(R.string.address_text,
+                            "No Place name found!",
+                            result, System.currentTimeMillis()));
+                }
+            });
         }
+    }
+
+    private void setPlaceTypeImage(Place currentPlace) {
+        int drawableID = R.drawable.android_plain;
+
+        List<Place.Type> types = currentPlace.getTypes();
+        if (types != null) {
+            for (Place.Type placeType : currentPlace.getTypes()) {
+                switch (placeType) {
+                    case SCHOOL:
+                        drawableID = R.drawable.android_school;
+                        break;
+                    case GYM:
+                        drawableID = R.drawable.android_gym;
+                        break;
+                    case RESTAURANT:
+                        drawableID = R.drawable.android_restaurant;
+                        break;
+                    case LIBRARY:
+                        drawableID = R.drawable.android_library;
+                        break;
+                }
+            }
+        }
+
+        mImageView.setImageResource(drawableID);
     }
 }
